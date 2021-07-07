434b6afc7a0e9f9b78670402180a503510ead1bf
Cuong Nghiem <quangcuongnghiem@gmail.com>
2021-07-07 09:11:21 +0700

    init

---
+++ src/Components/Login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../CSS/Login.css';
+
+class Login extends React.Component()
+{
+    constructor(props)
+    {
+        super(props);
+        this.props = props;
+    }
+
+    render()
+    {
+        // return jsx here
+    }
+}
+
+export default Login;
---
+++ src/Components/Register.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../CSS/Register.css';
+
+class Register extends React.Component()
+{
+    constructor(props)
+    {
+        super(props);
+        this.props = props;
+    }
+
+    render()
+    {
+        // return jsx here
+    }
+}
+
+export default Register;
