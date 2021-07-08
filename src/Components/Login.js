6d2b9fcd6fac63926a8c2ae30a5f6a17f1a338c8
Foot <phuvo2105@gmail.com>
2021-07-08 16:41:36 +0700

    Fix login logic

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -7,11 +7,44 @@ class Login extends React.Component
     {
         super(props);
         this.props = props;
-        this.state = {};
+        this.onUsernameChange = this.onUsernameChange.bind(this);
+        this.onPasswordChange = this.onPasswordChange.bind(this);
+        this.sendInfo = this.sendInfo.bind(this);
+
+        this.state = {
+            usernameValid: true,
+            passwordValid: true,
+        };
     }
 
-    sendInfo(event) {
-        console.log(event)
+    onUsernameChange(e) {
+        this.setState({
+            username: e.target.value,
+            usernameValid: !!e.target.value
+        });
+    }
+    
+    onPasswordChange(e) {
+        this.setState({
+            password: e.target.value,
+            passwordValid: !!e.target.value
+        });
+    }
+
+    sendInfo() {
+        console.log('Username:');
+        console.log(this.state.username);
+        console.log('Password');
+        console.log(this.state.password);
+
+        const usernameValid = !!this.state.username;
+        const passwordValid = !!this.state.password;
+        // Update the messages
+        this.setState({usernameValid, passwordValid});
+
+        if (usernameValid && passwordValid) {
+            // If the username & password are both valid, submit the form...
+        }
     }
 
     render()
@@ -20,11 +53,13 @@ class Login extends React.Component
         return (
             <div className='loginform'>
                 <p>Username</p>
-                <input type="text" className="username" id="username" name="username" placeholder="Enter your username"/>
+                <input type="text" className="username" id="username" name="username" onInput={this.onUsernameChange} placeholder="Enter your username"/>
+                {!this.state.usernameValid && <p>Username cannot be blank</p>}
                 <p>Password</p>
-                <input type="password" className="password" id="password" name="password" placeholder="Enter your password" type="password"/>
+                <input type="password" className="password" id="password" name="password" onInput={this.onPasswordChange} placeholder="Enter your password" type="password"/>
+                {!this.state.passwordValid && <p>Password cannot be blank</p>}
                 <br/>
-                <button onClick={this.sendInfo} type="button">Login</button>
+                <button onClick={this.sendInfo} type="submit">Login</button>
             </div>
         )
     }
